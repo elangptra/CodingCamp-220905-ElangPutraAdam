b22aa109a2bcff86876767f2610b6799c51efa0e
Adam <elangptra17@gmail.com>
2025-09-26 19:19:18 +0700

    feat: Name for Landing Page

--- js/script.js
+++ js/script.js
@@ -1,3 +1,26 @@
 function toggleMenu() {
   document.getElementById("mobile-menu").classList.toggle("hidden");
 }
+
+const heroTitle = document.getElementById("hero-title");
+const nameModal = document.getElementById("name-modal");
+const nameInput = document.getElementById("name-input");
+const nameSubmit = document.getElementById("name-submit");
+
+// Tampilkan modal saat pertama kali buka
+window.addEventListener("load", () => {
+  nameModal.classList.remove("hidden");
+  nameModal.classList.add("flex");
+});
+
+// Saat tombol "Masuk" diklik
+nameSubmit.addEventListener("click", () => {
+  const name = nameInput.value.trim();
+
+  if (name) {
+    heroTitle.textContent = `Hi ${name}, Welcome To My Persona Fansite`;
+    nameModal.classList.add("hidden");
+  } else {
+    alert("Nama tidak boleh kosong!");
+  }
+});
